3977ebc8c64de775b8d89594f19fb89bbcc627cd
Francis <bilodeaurobillardfrancis@gmail.com>
2020-05-17 23:54:46 -0400

    Refactored variable names and main function

--- Day03/day3.py
+++ Day03/day3.py
@@ -28,50 +28,50 @@ def minSteps(wire1, wire2):
 def positions(wire):
     board = [(0, 0)]
     for instruction in wire:
-        # Instruction example: R75,D30,R83,U83,L12,D49,R71,U7,L72
-
         # Instead of calculating the line, just enumerate all
         # positions.
         direction = instruction[0]
-        # Take the end of the string since the occurence can be
+        # Take the end of the string since the occurrence can be
         # more than one char.
-        occurence = int(instruction[1:])
+        occurrence = int(instruction[1:])
         lastX = board[-1][0]
         lastY = board[-1][1]
 
         if direction == "U":
-            for n in range(occurence):
+            for n in range(occurrence):
                 lastY += 1
                 board.append((lastX, lastY))
         elif direction == "D":
-            for n in range(occurence):
+            for n in range(occurrence):
                 lastY -= 1
                 board.append((lastX, lastY))
         elif direction == "L":
-            for n in range(occurence):
+            for n in range(occurrence):
                 lastX -= 1
                 board.append((lastX, lastY))
         elif direction == "R":
-            for n in range(occurence):
+            for n in range(occurrence):
                 lastX += 1
                 board.append((lastX, lastY))
     return board
 
-with open(os.path.join(sys.path[0], "input.txt"), "r") as f:
-    # First, store both wire in lists.
-    wires = []
-    for line in f:
-        wires.append(line.split(","))
 
-    # Map all positions for both wires
-    wire1 = positions(wires[0])
-    wire2 = positions(wires[1])
+if __name__ == '__main__':
+    with open(os.path.join(sys.path[0], "input.txt"), "r") as f:
+        # First, store both wire in lists.
+        wires = []
+        for line in f:
+            wires.append(line.split(","))
 
-    # PART 1
-    # Find all all the intersections and print the smallest one.
-    print(minDistance(wire1, wire2))
+        # Map all positions for both wires
+        wire1 = positions(wires[0])
+        wire2 = positions(wires[1])
 
-    # PART 2
-    # Use the same logic as in part one but return the smallest
-    # number of steps.
-    print(minSteps(wire1, wire2))
+        # PART 1
+        # Find all all the intersections and print the smallest one.
+        print(minDistance(wire1, wire2))
+
+        # PART 2
+        # Use the same logic as in part one but return the smallest
+        # number of steps.
+        print(minSteps(wire1, wire2))
