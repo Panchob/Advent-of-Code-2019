df45aa410e628f4323b025c0f483853b8603f4d9
Francis <bilodeaurobillardfrancis@gmail.com>
2020-05-30 23:27:35 -0400

    Feat: Added a function Now trying to list all possible valid paths.

--- Day15/day15.py
+++ Day15/day15.py
@@ -1,35 +1,27 @@
 import os
 import sys
-import queue
 from intCode_compiler import Intcode
 
-class Node:
-    def __init__(self, position, path=[]):
-        self.position = position
-        self.visited = False
-        self.path = path
-
 DIRECTIONS = [1,2,3,4]
-# Use a queue to visited all that is near current node first.
-actions = queue.Queue()
+path = []
+validPaths = []
+visited = []
+
+
+class Node:
+    def __init__(self, position, lastDirection):
+        self.position = position
+        self.lastDirection = lastDirection
     
-if __name__ == "__main__":
-    with open(os.path.join(sys.path[0], "input.txt"), "r") as f:
-
-        l = f.read().split(",")
-        l = list(map(int, l))
-
-        padding =  [0] * 1000
-        memory = l + padding
-
-        intcode = Intcode(memory)
-
-        current = Node((0,0))
-        current.visited = True
-
-        # Create first nodes and paths
-        for direction in DIRECTIONS:
-            x,y = current.position
+#List all possible paths
+def updateGraph(current, intCode):
+    visited.append(current.position)
+    for direction in DIRECTIONS:
+        if direction == reverse(current.lastDirection):
+            continue
+        result = intCode.run(direction)
+        x, y = current.position
+        if result != 0:
             if direction == 1:
                 y += 1
             elif direction == 2:
@@ -39,6 +31,42 @@ if __name__ == "__main__":
             elif direction == 4:
                 x += 1
 
-            path = current.path
+            if result == 2:
+                print(x,y)
+                validPaths.append(len(path))
+                break
+                
             path.append(direction)
-            actions.put(Node((x,y), path))
+            updateGraph(Node((x,y), direction), intCode)
+            
+    if path:
+        path.pop()
+        intCode.run(reverse(current.lastDirection))
+    
+
+        
+def reverse(direction):
+    r = 0
+    if direction == 1:
+        r = 2
+    elif direction == 2:
+        r = 1
+    elif direction == 3:
+        r = 4
+    elif direction == 4:
+        r = 3
+    return r
+    
+if __name__ == "__main__":
+    with open(os.path.join(sys.path[0], "input.txt"), "r") as f:
+        l = f.read().split(",")
+        l = list(map(int, l))
+
+        padding =  [0] * 1000
+        memory = l + padding
+        intCode = Intcode(memory)
+
+        current = Node((0,0),0)
+        updateGraph(current, intCode)
+
+    print(validPaths)
