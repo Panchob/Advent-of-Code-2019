c94288b522ab8db49f35b33b58c0957296628fd2
Francis <bilodeaurobillardfrancis@gmail.com>
2020-07-21 15:38:21 -0400

    Refactor: removed some comment in day one

--- Day01/day1.py
+++ Day01/day1.py
@@ -2,28 +2,30 @@ import math
 import os
 import sys
 
-# The required fuel is the mass of the module
-# divided by three, rounded down and substracted by 2.
 def formula(module_mass):
     return math.floor(int(module_mass)/3 - 2)
 
 
 def fuelMass(mass):
-    # If it's less than it's negligible.
     if mass <= 0:
         return 0
     else:
         return mass + fuelMass(formula(mass))
 
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
+    part1 = 0
+    part2 = 0
+
     with open(os.path.join(sys.path[0], "input.txt"), "r") as f:
-        part_1 = 0
-        part_2 = 0
 
         for module in f.readlines():
-            part_1 += formula(module)
-            part_2 += fuelMass(formula(module))
+            # Get the total mass of fuel used to send the rocket
+            # in orbit.
+            part1 += formula(module)
+            
+            # Same thing, but including the fuel mass as well.
+            part2 += fuelMass(formula(module))
 
-        print("PART 1:", part_1)
-        print("PART 2:", part_2)
+    print("PART 1:", part1)
+    print("PART 2:", part2)
