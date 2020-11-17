cca6a0ee146627eeca00ba7fd1a6dd5ba21d1bc8
Francis <bilodeaurobillardfrancis@gmail.com>
2020-11-17 10:20:35 -0500

    feat: day 17, 19 and 24 rough solutions

--- Day17/day17.py
+++ Day17/day17.py
@@ -1,44 +1,101 @@
 import os
 import sys
+from collections import defaultdict
 # Add the parent directory to the path
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
-from intcode.intCode_compiler import Intcode
+from Intcode.Intcode import Intcode
 
-def listIntersection(graph):
-    intersection = set()
+class Graph():
+    def __init__(self, height):
+        self.__height = height
+        self.nodes = defaultdict(list)
+        self.alignment = 0
 
-    for node in graph:
+
+    def calibrate(self):
+        for node, adjacents in self.nodes.items():
+            if len(adjacents) == 4:
+                x, y = node
+                self.alignment += (self.__height - y) * x
+
+
+    def listAdjacents(self, node):
         x, y = node
-        adjacents = [(x- 1, y), (x, y + 1), (x + 1, y), (x, y - 1)]
-        if all(a in graph for a in adjacents):
-            intersection.add((x - 1, y))
-    
-    return (intersection)
+        adjacents = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+        for a in adjacents:
+            if a in self.nodes.keys():
+                self.nodes[node].append(a)
 
 
-with open(os.path.join(sys.path[0], "output.txt"), "w") as w:
+if __name__ == "__main__":
     intcode = Intcode("input.txt")
-    w.write(" ")
+    intcode.run()
+    out = intcode.getOutput()
 
-    graph = set()
-    x = 0
-    y = 0
-    while not intcode.stopped:
-        num = intcode.run()
-        if num == 10:
-            y += 1
+
+    length = 1
+    with open(os.path.join(sys.path[0], "output.txt"), "w") as w:
+        for n in out:
+            if n == 10:
+                length += 1
+            w.write(chr(n))
+    
+    graph = Graph(length)
+    with open(os.path.join(sys.path[0], "output.txt"), "r") as r:
+        x, y = (0, length)
+        for line in r.readlines():
+            y -= 1
             x = 0
-        else:
-            x += 1
-            if num == 35:
-                graph.add((x, y))
+            for c in line:
+                x += 1
+                if c == "#":
+                    graph.nodes[(x, y)] = []
 
-        w.write(chr(num))
-        w.write(" ")
+    for node in graph.nodes.keys():
+        graph.listAdjacents(node)
 
-intersections = listIntersection(graph)
-alignments = [i[0] * i[1] for i in intersections]
-print(intersections)
-print(alignments)
-print(sum(alignments))
+    graph.calibrate()
+    print(graph.alignment)
+
+    A = [ord(c) for c in "L,12,L,10,R,8,L,12\n"]
+    B = [ord(c) for c in"R,8,R,10,R,12\n"]
+    C = [ord(c) for c in "L,10,R,12,R,8\n"]
+    mainRoutine = [ord(c) for c in "A,B,A,B,C,C,B,A,B,C\n"]
+
+    intcode = Intcode("input_part2.txt")
+    intcode.run()
+    out = intcode.getOutput(ascii=True)[-6:-1]
+    print("".join(out), mainRoutine)
+
+    for i in mainRoutine:
+        intcode.run(i)
+    
+    out = intcode.getOutput(ascii=True)
+    print("".join(out), A)
+    for i in A:
+        intcode.run(i)
+
+    out = intcode.getOutput(ascii=True)
+    print("".join(out), B)
+    for i in B:
+        intcode.run(i)
+    
+    out = intcode.getOutput(ascii=True)
+    print("".join(out), C)
+    for i in C:
+        intcode.run(i)
+    
+
+    out = intcode.getOutput(ascii=True)
+    print("".join(out), "n")
+    intcode.run(ord("n"))
+    intcode.run(10)
+    out = intcode.getOutput(ascii=True)
+    print("".join(out))
+        
+
+
+
+
+
